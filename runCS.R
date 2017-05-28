ede6071e9ca6a226aead64f1dfed5dab663ba818
rojaff <r.jaffe@ib.usp.br>
2017-05-28 11:14:27 -0300

    Update runCS.R

--- runCS.R
+++ runCS.R
@@ -18,6 +18,12 @@ runCS <- function(coordinates, raster){
   system(CS_run)
 }
 
+#### Create tab-separated file containing coordinates:
+# Coords <- read.csv("Coords.csv", head=T) ## read csv file containing coordinates
+# Coords$ID <- 1:nrow(Coords) ## append row ID
+# Coords <- Coords [ , c(3,1,2)] ## Select IDs and coordinates
+# write.table(Coords, file="coordinates.txt", sep="\t", row.names=F, col.names=F) ## write tab-separated file
+
 ### Example use:
 ## > source("runCS.R")
 ## > runCS(coordinates="coordinates.txt", raster="GD.asc")
